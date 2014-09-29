ae2292774e4c58e99f81ec70c856616f29b0d7fa
Alastair Hughes <hobbitalastair@yandex.com>
2014-09-29 16:53:24 +1300

    Fixed renderer...

--- renderer.py
+++ renderer.py
@@ -5,6 +5,8 @@ from thingy import Thingy
 
 display = pygame.display.set_mode()
 
+display.fill()
+
 thing1 = Thingy(x=5, y=6, rotation=0)
 
 def render(thingy, display):
@@ -12,7 +14,14 @@ def render(thingy, display):
 
     midpoint = display.get_height() // 2
 
-    display.blit(thing1.image, Rect(0, 0, thing1.width, thing1.height))
+    height = thingy.image.get_heigth()
+    width = thingy.image.get_width()
+    x = thingy.x
+    y = midpoint - thingy.y
+
+    coords = Rect(x, y, width, height)
+
+    display.blit(thingy.image, coords)
 
 render(thingy, display)
 
